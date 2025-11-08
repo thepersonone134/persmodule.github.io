bf4d2e6eb164b7900cd2a5ba575efa9aa7c903d3
thepersonone134 <tinaxue4@gmail.com>
2025-11-08 19:33:20 +0000

    progresssssss

--- src/backend.js
+++ src/backend.js
@@ -1,20 +1,21 @@
-const elements = document.getElementsByClassName("sidebuttons");
-const subsections = document.getElementsByClassName("SubSection");
-
-function handleButtonInteraction(element) {
-    var associatedElements = document.getElementById(element.id)
-    for (let i = 0; i < subsections.length; i++) {
-        subsections[i].style.display = 'none';
-    }
-    for (let i = 0; i < associatedElements.length; i++) {
-        if (associatedElements[i].class != "sidebuttons") {
-            associatedElements[i].style.display = 'block';
-        }
-    }
-}
+var elements = document.querySelectorAll(".sidebuttons");
+var subsections = document.querySelectorAll(".SubSection");
 
 for (let i = 0; i < elements.length; i++) {
     if (elements[i].id) {
-       elements[i].onclick = handleButtonInteraction(elements[i])
+        elements[i].addEventListener("click", 
+            function() {
+                var element = elements[i]
+                var associatedElements = document.querySelectorAll("#"+(element.id))
+                for (let i = 0; i < subsections.length; i++) {
+                    subsections[i].style.display = 'none';
+                }
+                for (let i = 0; i < associatedElements.length; i++) {
+                    if (associatedElements[i].class != "sidebuttons") {
+                        associatedElements[i].style.display = 'block';
+                    }
+                }
+            }
+        )
     }
 }
