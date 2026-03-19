508eb69dc205f08f9a92d4e7cb727f861918129c
thepersonone134 <tinaxue4@gmail.com>
2026-03-19 23:01:32 +0000

    Redid tweening in backend, and added more information in docs

--- src/backend.js
+++ src/backend.js
@@ -1,127 +1,184 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-var elements = document.querySelectorAll(".sidebuttons");
-var BackToTop = document.querySelectorAll("#BackToTop")
+const TweenGenericSetters = {
+    "colour": function(object, changes) {
+        object.style.backgroundColor = `rgb(${changes[0]},${changes[1]},${changes[2]})`;
+    },
+    "opacity": function(object, changes) {
+        object.style.opacity = changes[0];
+    },
+    "pagepos": function(object, changes, org) {
+        object.scrollTop = changes[0];
+    },
+}
 
-var pastsectiondata = "introduction";
-var pastsectiontext = "Introduction";
+const ProtectedDocuments = ["core","clients","mousefollow","riggermodule","downloader"]
 
-async function TweenColour(div, destination, time) {
-    let styles = window.getComputedStyle(div);
-    let backgroundcolour = styles.backgroundColor;
+var PastData = {
+    "SectionData": "introduction",
+    "SectionText": "Introduction",
+}
 
-    let c = backgroundcolour.split(",")
+var Elements = document.querySelectorAll(".sidebuttons");
+var BackToTop = document.querySelectorAll("#BackToTop");
+
+class TweenInfo {
+    constructor(time, easingstyledirection) {
+        this.EasingCombos = {
+            "SineIn": function(percentile) {
+                return 1 - Math.cos(percentile * (Math.PI/2));
+            },
+            "SineOut": function(percentile) {
+                return Math.sin(percentile * (Math.PI/2));
+            },
+            "SineInOut": function(percentile) {
+                return (-1/2) * (Math.cos(Math.PI * percentile) - 1);
+            },
+            "Linear": function(percentile) {
+                return percentile;
+            }
+        };
+
+        return {
+            "time": time,
+            "easing": [this.EasingCombos, easingstyledirection]
+        };
+    };
+}
+
+class Tween {
+    constructor(object, tweeninfo, goal, org, setter) {
+        this.object = object;
+        this.tweendata = tweeninfo;
+        this.goal = goal;
+        this.org = org;
+        this.setter = setter;
+        this.init = null;
+
+        this.step = this.step.bind(this);
+    }
+    step(elapsed) {
+        if (this.init == null) {
+            this.init = elapsed;
+        };
+        let percentiletravel = (elapsed-this.init)/(this.tweendata.time*1000);
+        if (percentiletravel >= 1) {
+            this.setter(this.object, this.goal);
+            return;
+        }
+
+        let changes = [];
+        for (let i = 0; i < this.org.length; i++) {
+            let asgoal = this.goal[i];
+            let asorg = this.org[i];
+
+            changes[i] = asorg + ((asgoal-asorg) * this.tweendata.easing[0][this.tweendata.easing[1]](percentiletravel));
+        }
+        
+        this.setter(this.object, changes, this.org);
+
+        requestAnimationFrame(this.step)
+    }
+    play() {
+        requestAnimationFrame(this.step)
+    };
+}
+
+function threevariables(object) {
+    let styles = window.getComputedStyle(object);
+    let bgclr = styles.backgroundColor;
+    let c = bgclr.split(",")
     let r = String(c[0]); let g = String(c[1]); let b = String(c[2]);
 
-    r = Number(r.split("rgb(")[1]); g = Number(g); b = Number(b.split(")")[0])
-
-    let rr = (destination[0]-r); let rg = (destination[1]-g); let rb = (destination[2]-b)
-
-    let absStart = performance.now();
-    function LoopLogicColour() {
-        let start = performance.now();
-
-        if (time*1000 < start-absStart) {
-            return
-        }
-
-        let timeleft = 1-(((time*1000)-(start-absStart))/(time*1000))
-
-        div.style.backgroundColor = "rgb("+(r+(rr*timeleft))+","+(g+(rg*timeleft))+","+(b+(rb*timeleft))+")";
-
-        requestAnimationFrame(LoopLogicColour);
-    }
-    LoopLogicColour()
+    return [Number(r.split("rgb(")[1]), Number(g), Number(b.split(")")[0])];
 }
 
-async function TweenOpacity(div, destination, time) {
-    let styles = window.getComputedStyle(div);
-    let opacity = Number(styles.opacity);
-
-    let diffopacity = destination-opacity
-
-    let absStart = performance.now();
-    function LogicLoop() {
-        let start = performance.now();
-
-        if (time*1000 < start-absStart) {
-            return
-        }
-
-        let timeleft = 1-(((time*1000)-(start-absStart))/(time*1000))
-
-        div.style.opacity = opacity + (diffopacity*timeleft);
-
-        requestAnimationFrame(LogicLoop);
-    }
-    LogicLoop()
-}
-
-async function TweenPagePos(time) {
-    let scrollpos = document.getElementById("Holder").scrollTop
-    let absStart = performance.now();
-
-    function LogicLoop() {
-        let start = performance.now();
-
-        if (time*1000 < start-absStart) {
-            document.getElementById("Holder").scrollTop = 0
-            return
-        }
-
-        let timeleft = 1-(((time*1000)-(start-absStart))/(time*1000))
-
-        document.getElementById("Holder").scrollTop = scrollpos + (-scrollpos*timeleft);
-
-        requestAnimationFrame(LogicLoop);
-    }
-    LogicLoop()
-}
-
-
-BackToTop[0].addEventListener("click", 
+BackToTop[0].addEventListener("click",
     function() {
-        TweenPagePos(.3)
+        let tweeninfo = new TweenInfo(1, "SineInOut")
+        let tween = new Tween(document.getElementById("Holder"), tweeninfo, [0], [document.getElementById("Holder").scrollTop], TweenGenericSetters.pagepos)
+
+        tween.play()
     }
 )
 
-elements.forEach(element => {
-    element.addEventListener("click",
+let confirm = document.getElementById("overlayconfirm");
+let options = document.getElementById("overlayoptions");
+
+let acceptrisk = document.getElementById("AcceptRiskButtonPrompt");
+let rejectrisk = document.getElementById("RejectRiskButtonPrompt");
+
+acceptrisk.addEventListener("click",
+    function() {
+        let noticetweeninfo = new TweenInfo(.3, "SineOut")
+        new Tween(confirm, noticetweeninfo, [1], [confirm.style.opacity], TweenGenericSetters.opacity);
+
+        delay(300);
+
+        confirm.style.display = "none";
+        options.style.display = "none";
+    }
+)
+
+rejectrisk.addEventListener("click",
+    function() {
+        window.location.replace('https://github.com/');
+    }
+)
+
+Debounce = false;
+Elements.forEach(element => {
+    element.addEventListener("click", 
         function() {
-            let pastline = document.querySelectorAll(".aLine."+pastsectiondata);
-            let pastsubsection = document.querySelectorAll(".subSection."+pastsectiondata);
+            if (Debounce == true) { return; }
+            Debounce = true;
 
-            TweenColour(pastline[0], [126, 135, 145], .3);
-            TweenOpacity(pastsubsection[0], -1, .3);
+            let newData = element.className.split(" ")[2];
 
-            document.querySelectorAll(".sidebuttons."+pastsectiondata)[0].innerHTML = pastsectiontext;
+            if (ProtectedDocuments.includes(newData)) {
+                let noticetweeninfo = new TweenInfo(.3, "SineInOut")
+                confirm.style.display = "block";
+                options.style.display = "flex";
+                new Tween(confirm, noticetweeninfo, [0.8], [confirm.style.opacity], TweenGenericSetters.opacity);
+            }
 
-            let sectiondata = element.className.split(" ")[2];
-            let line = document.querySelectorAll(".aLine."+sectiondata);
-            let subsection = document.querySelectorAll(".subSection."+sectiondata);
+            let pastLine = document.querySelector(`.aLine.${PastData.SectionData}`);
+            let pastSub = document.querySelector(`.subSection.${PastData.SectionData}`);
+
+            let tweenpastinfo = new TweenInfo(.3, "SineIn")
+            
+            new Tween(pastLine, tweenpastinfo, [126,135,145], threevariables(pastLine), TweenGenericSetters.colour).play();
+            new Tween(pastSub, tweenpastinfo, [0], pastSub.style.opacity, TweenGenericSetters.opacity).play();
+
+            document.querySelector(`.sidebuttons.${PastData.SectionData}`).innerHTML = PastData.SectionText;
+
+            let newLine = document.querySelector(`.aLine.${newData}`);
+            let newSub = document.querySelector(`.subSection.${newData}`);
 
             delay(300);
 
-            pastsubsection[0].style.display = "none";
+            pastSub.style.display = "none";
 
-            pastsectiondata = sectiondata
-            pastsectiontext = element.innerHTML
+            PastData.SectionData = newData;
+            PastData.SectionText = element.innerHTML
 
-            element.innerHTML = "<b style=\"color: rgb(255,255,255);\">"+pastsectiontext+"</b>"
+            element.innerHTML = `<b style="color: rgb(255,255,255);">${PastData.SectionText}</b>`
+            let tweennewinfo = new TweenInfo(.3, "SineOut")
 
-            TweenColour(line[0], [255,255,255], .3)
-            TweenOpacity(subsection[0], 2, .3)
+            new Tween(newLine, tweennewinfo, [255,255,255], threevariables(newLine), TweenGenericSetters.colour).play();
+            new Tween(newSub, tweennewinfo, [1], newSub.style.opacity, TweenGenericSetters.opacity).play();
 
-            subsection[0].style.display = "inline-block";
-            if (subsection[0].scrollHeight > subsection[0].clientHeight) {
+            newSub.style.display = "inline-block";
+            if (newSub.scrollHeight > newSub.clientHeight) {
                 BackToTop[0].style.display = "inline";
-                TweenOpacity(BackToTop[0], 2, .3);
+                new Tween(BackToTop[0], tweennewinfo, [1], [0], TweenGenericSetters.opacity).play();
             } else {
-                TweenOpacity(BackToTop[0], -1, .3);
-                delay(300)
+                new Tween(BackToTop[0], tweenpastinfo, [0], [1], TweenGenericSetters.opacity).play();
+                delay(300);
                 BackToTop[0].style.display = "none";
             }
-            subsection[0].style.display = "inline";
+            newSub.style.display = "inline";
+            Debounce = false;
         }
     )
-});
+})
